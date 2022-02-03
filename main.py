618cdd017118f0bd7fa511e6cee9f03fa4b0efef
pluto-Swallow <94426717+pluto-Swallow@users.noreply.github.com>
2022-02-03 14:48:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Author: your name
+Date: 2022-02-03 14:15:38
+LastEditTime: 2022-02-03 14:40:29
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \get_video_sydtd:\python_program\first_github\test1\main.py
+'''
+print("github")
+a = 3
+b = a
+a = 4
+print(a)
+print(b)
+
